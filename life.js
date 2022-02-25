9a20ced76d2ac08da94e1ccb4d9daffd6d19cb46
SX Spy Agent <90962949+SX-9@users.noreply.github.com>
2022-02-25 02:45:14 +0000

    E4

--- life.js
+++ life.js
@@ -1,5 +1,6 @@
 var welcome = "Hi, Welcome to SX's 1st Website!";
+var suprise = "Read The Console! ;)"
 
 console.log('System: ' + welcome);
 
-
+document.getElementById("secret").innerHTML = suprise;
