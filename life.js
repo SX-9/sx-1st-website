744c6c88add59e3e7a2870a6a1ac48e9c169e1a0
SX Spy Agent <90962949+SX-9@users.noreply.github.com>
2022-06-03 21:35:16 +0700

    new design and content

--- life.js
+++ life.js
@@ -1,3 +1,30 @@
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+});
+
+document.getElementById('web').onclick = () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            alert('Thanks for installing my pwa!');
+        }
+    });
+};
+
+document.getElementById('bot').onclick = () => {
+    window.open('https://top.gg/bot/889384219678232606');
+}
+
+document.getElementById('chat').onclick = () => {
+    window.open('https://x-cord-client.sx9.repl.co');
+}
+
+document.getElementById('email').onclick = () => {
+    window.open('mailto:sx-91@outlook.com');
+}
+
 document.getElementById('discord').onclick = () => {
     window.open('https://discord.gg/Pj2Qgqd');
 }
