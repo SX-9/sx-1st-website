379d544f92a1d6fae15788694dfa7bbd54a1709d
SX Spy Agent <90962949+SX-9@users.noreply.github.com>
2022-06-25 11:28:48 +0700

    Update life.js

--- life.js
+++ life.js
@@ -1,25 +1,5 @@
-//lazy loading css background images
 document.querySelectorAll('img').forEach(img => {
-    img.setAttribute('data-src', img.src);
-    img.removeAttribute('src');
     img.setAttribute('loading', 'lazy');
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
-    if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
-        let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
-        entries.forEach(function(entry) {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-                lazyBackgroundObserver.unobserve(entry.target);
-                }
-            });
-        });
-        lazyBackgrounds.forEach(function(lazyBackground) {
-            lazyBackgroundObserver.observe(lazyBackground);
-        });
-    }
 });
 
 const observer = new IntersectionObserver(entries => {
