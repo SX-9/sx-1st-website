9c94138af33fa99566f7e1ca7e1fd8104eaca913
NAME <mail@gmail.com>
2023-01-28 21:56:09 +0700

    function for later

--- life.js
+++ life.js
@@ -48,6 +48,18 @@ function glitch(el) {
 document.querySelectorAll(".glitch").forEach(glitch);
 document.querySelectorAll("button").forEach(glitch);
 
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+×=/_!@#$%^&*()-":;,?`~\|<>{}[]';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
 const typingChange = () => {
   const typing = document.getElementById("typing");
   setTimeout(() => (typing.innerText = "Developer"), 500);
