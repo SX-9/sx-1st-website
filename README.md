e4fd34878aae75ab244607cd279a4aec3665bfe4
sx9dev <sx-91@outlook.com>
2023-01-28 15:42:14 +0700

    redesign

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 ## My 1st Website!
 Links:
 * [Main Site](https://sx9.is-a.dev/)
-* [Backup Site](https://sx9-1st.web.app)
-
-*The Backup Site Is A Clone Of The Main Site Just In Case It Goes Offline*
--- life.js
+++ life.js
@@ -1,65 +1,101 @@
 if (window.innerWidth < 380) {
-    let answear = prompt("Your Device Is Too Small, Are You Sure You Want To View This Page?\n\nType 'yes' to continue.");
-    if (answear.toLowerCase() !== "yes") {
-        document.body.style.display = 'none';
-    } else {
-        document.body.style.display = 'block';
-    }
+  let answear = prompt(
+    "Your Device Is Too Small, Are You Sure You Want To View This Page?\n\nType 'yes' to continue."
+  );
+  if (answear.toLowerCase() !== "yes") {
+    document.body.style.display = "none";
+  } else {
+    document.body.style.display = "block";
+  }
 }
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('scroll');
-        }
-    });
-});
-let scrollItems = document.querySelectorAll('div.container');
-scrollItems.forEach(item => {
-    observer.observe(item);
-});
 
 var install;
-window.addEventListener('beforeinstallprompt', e => {
-    e.preventDefault();
-    install = e;
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  install = e;
 });
 
-const typingChange = () => {
-    const typing = document.getElementById('typing');
-    setTimeout(() => typing.innerText = 'Developer', 500);
-    setTimeout(() => typing.innerText = 'Tech Enthusist', 1500);
-    setTimeout(() => typing.innerText = 'Cat Lover', 2500);
-    setTimeout(() => typing.innerText = 'Fake Hacker', 3500);
-    setTimeout(() => typing.innerText = 'Arch User', 4500);
-    setTimeout(() => typing.innerText = 'Secret Agent', 5500);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+function glitch(el) {
+  el.onmouseover = (event) => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+  };
 }
+document.querySelectorAll(".glitch").forEach(glitch);
+document.querySelectorAll("button").forEach(glitch);
+
+const typingChange = () => {
+  const typing = document.getElementById("typing");
+  setTimeout(() => (typing.innerText = "Developer"), 500);
+  setTimeout(() => (typing.innerText = "Tech Enthusist"), 1500);
+  setTimeout(() => (typing.innerText = "Cat Lover"), 2500);
+  setTimeout(() => (typing.innerText = "Fake Hacker"), 3500);
+  setTimeout(() => (typing.innerText = "Arch User"), 4500);
+  setTimeout(() => (typing.innerText = "Secret Agent"), 5500);
+};
 
 typingChange();
 setInterval(typingChange, 7000);
 
-fetch('https://visitors.sx9.repl.co/').then(res => res.json()).then(data => {
+fetch("https://visitors.sx9.repl.co/")
+  .then((res) => res.json())
+  .then((data) => {
     console.log(data);
-    document.getElementById('counter').innerText = data.visits + "th";
-}).catch(err => {
+    document.getElementById("counter").innerText = data.visits + "th";
+  })
+  .catch((err) => {
     console.log(err);
-    document.getElementById('visits').remove();
-});
+    document.getElementById("visits").remove();
+  });
 
-document.getElementById('email').onclick = () => window.open('mailto:hello@mail.sx9.is-a.dev');
-document.getElementById('discord').onclick = () => window.open('https://discord.st/sx-aircraft');
-document.getElementById('github').onclick = () => window.open('https://github.com/SX-9');
-document.getElementById('twitter').onclick = () => window.open('https://twitter.com/SX_Disord');
-document.getElementById('share').onclick = () => window.open("https://twitter.com/share?url=" + window.location.href);
-document.getElementById('pwa').onclick = install.prompt;
+document.getElementById("email").onclick = () =>
+  window.open("mailto:hello@mail.sx9.is-a.dev");
+document.getElementById("discord").onclick = () =>
+  window.open("https://discord.st/sx-aircraft");
+document.getElementById("github").onclick = () =>
+  window.open("https://github.com/SX-9");
+document.getElementById("twitter").onclick = () =>
+  window.open("https://twitter.com/SX_Disord");
+document.getElementById("share").onclick = () =>
+  window.open("https://twitter.com/share?url=" + window.location.href);
+document.getElementById("pwa").onclick = install.prompt;
+
+document.getElementById("up").onclick = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
 console.log(
-    "%cHello%cWorld()", 
-    "color: cyan; background-color: black; font-family: sans-serif; font-weight: bold;", 
-    "color:red; background-color: white; font-family: sans-serif; font-weight: bold;"
+  "%cHello%cWorld()",
+  "color: cyan; background-color: black; font-family: sans-serif; font-weight: bold;",
+  "color:red; background-color: white; font-family: sans-serif; font-weight: bold;"
 );
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/pwa/sw.js');
-    console.log("Service Worker Registered!");
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/pwa/sw.js");
+  console.log("Service Worker Registered!");
 }
